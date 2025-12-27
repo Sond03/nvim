54edbc79e6cdfbf2d6177c20b8ebe29acf8c0d1a
sondre <sondret321@gmail.com>
2025-12-27 04:42:05 +0100

    barbar keybinds and shows when you copy text

--- lua/sond/init.lua
+++ lua/sond/init.lua
@@ -1,3 +1,11 @@
 require("sond.remap")
 require("sond.lazy")
 require("sond.set")
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
--- lua/sond/remap.lua
+++ lua/sond/remap.lua
@@ -79,3 +79,34 @@ end)
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+-- barbar keymaps
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+map('n', '<A-h>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<A-l>', '<Cmd>BufferNext<CR>', opts)
+-- Alt . prev , next
+
+-- Goto tab in position...
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+-- Alt 1-0
+
+-- Close open tab with Alt c
+map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+
+-- Pin tabs with Alt p
+map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
