04a0798c16e8101ce8a52cde33b56c17229439c7
sondre <sondret321@gmail.com>
2026-06-25 00:03:12 +0200

    added settings to colorizer.lua

--- lua/sond/plugins/colorizer.lua
+++ lua/sond/plugins/colorizer.lua
@@ -1,5 +1,16 @@
 return {
-    "catgoose/nvim-colorizer.lua", enabled = false;
+    "catgoose/nvim-colorizer.lua", enabled = true;
     event = "BufReadPre",
-    opts = {},
+    opts = {
+        RGB      = true;         -- #RGB hex codes
+        RRGGBB   = true;         -- #RRGGBB hex codes
+        names    = false;         -- "Name" codes like Blue
+        RRGGBBAA = true;        -- #RRGGBBAA hex codes
+        rgb_fn   = true;        -- CSS rgb() and rgba() functions
+        hsl_fn   = false;        -- CSS hsl() and hsla() functions
+        css      = false;        -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn   = false;        -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        -- Available modes: foreground, background
+        mode     = 'background'; -- Set the display mode.
+    },
 }
